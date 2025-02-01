bf6a3f484a315be1ca33849debe45eed16c6544e
Aryan <nandaaryan823@gmail.com>
2025-02-01 16:20:20 +0530

    Week3 done
    Signed-off-by: Aryan <nandaaryan823@gmail.com>

---
+++ Week3/main1.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://admin:9P%40LwV9Z6Sgh_VE@cluster0.mc4hu.mongodb.net/usersappnew");
+
+const User = mongoose.model('Users', {name: String, email: String, password: String}); // this defines a document structure using schema
+
+app.post("/users", async (req,res) => {
+    const username = req.body.username;
+    const email = req.body.username;
+    const password = req.body.password;
+
+    let user_exist = await User.findOne({name: username});
+    if(user_exist){
+        return res.status(404).send("Username already exists!");
+    } 
+    const user = new User({name: username, email: email, password: password}); // creates a new document of type User
+    user.save();
+    res.send("Users created successfully!");
+});
+
+app.listen(3000);
